771afab719c78e8a9e40fe739fa55a29d19fa716
Somaya Tabassum <92995183+TabassumS9@users.noreply.github.com>
2022-12-10 10:01:34 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,44 @@
+$(function(){
+    // slider
+    $('#banner').slick({
+        arrows:false,
+        dots:true,
+    }
+    );
+    $(".Product_slider").slick({
+        slidesToShow: 4,
+        prevArrow:'.arrows .left',
+        nextArrow:'.arrows .right',
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 4,
+                
+              }
+            },
+            {
+                breakpoint: 991,
+                settings: {
+                  slidesToShow: 3,
+                  
+                }
+              },
+            {
+              breakpoint: 767,
+              settings: {
+                slidesToShow: 2,
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+        });
+    });
